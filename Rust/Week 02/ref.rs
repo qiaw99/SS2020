a7002c2f9e3cabbc0db37a3da98b14c4b2b2df2b
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-04-30 07:16:56 +0200

    Create ref.rs

---
+++ Rust/Week 02/ref.rs
@@ -0,0 +1,32 @@
+// My first project
+
+fn main() {
+	let s1 = String::from("hello");
+	let len = calculate_length_2(&s1);
+	println!("The length of '{}' is {}", s1, len);
+	let (s3, len) = calculate_length_1(s1);
+
+	let mut s = String::from("hello");
+	change(&mut s);
+	println!("The changed string is: {}", s);
+}
+
+/* 
+   Change the string(mutable reference). 
+   However, in a specific scope, only one
+   mutable reference is allowed. Solution is
+   we can create more scope by using {}
+*/
+fn change(some_string: &mut String){
+	some_string.push_str(", world");
+}
+
+// reference -> borrowing
+fn calculate_length_1(s: String) -> (String, usize){
+	let length = s.len();
+	return (s, length);
+}
+
+fn calculate_length_2(s: &String) -> usize{
+	return s.len();
+}
