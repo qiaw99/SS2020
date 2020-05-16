624b3685747a6727d80260904547f34b995b476f
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-05-16 13:13:25 +0200

    Update U4_A1.c

--- ALP4/U4/U4_A1.c
+++ ALP4/U4/U4_A1.c
@@ -5,28 +5,25 @@
 #include <semaphore.h>
 #include <math.h>
 
-//#define THINKING 0
-//#define HUNGRY 1
-//#define EATING 2
+int *count;
 
-//int *chop;
 int num_threads;
-
-pthread_mutex_t lock;
 
 sem_t mutex;
 
 void *philosopher(void *threadid){
 	long tid = (long)threadid;
-
 	for(int i = 0; i < 1000; i++){
 		printf("%ld is thinking...\n", tid);
-		sleep(1);
+		sleep(2);
 
 		sem_wait(&mutex);
+		count[tid] ++;
 		printf("%ld is dinning\n", tid);
 		sleep(1);
+		
 		printf("%ld finished.\n", tid);
+		sleep(1);
 		sem_post(&mutex);
 	}
 }
@@ -41,12 +38,20 @@ int main(int argc, char *argv[]){
 
     pthread_t threads[num_threads];
     
-    sem_init(&mutex, 0, floor(num_threads / 2));
+    // Initializieren die Anzahl von Stäbchen
+    if(num_threads == 1){
+    	sem_init(&mutex, 0, 1);
+    }else if(num_threads < 0){
+    	exit(-1);
+    }else{
+ 	   sem_init(&mutex, 0, floor(num_threads / 2));
+ 	}
 
-    // Lock initialisieren
-    pthread_mutex_init(&lock, NULL);
-
-    //chop = (int *)malloc(sizeof(int) * num_threads);
+ 	// Initialisieren count
+    count = (int *)malloc(sizeof(int) * num_threads);
+    for(int i = 0; i < num_threads; i++){
+    	count[i] = 0;
+    }
     
     //creating threads
     for(t = 0; t < num_threads; t++){
@@ -63,8 +68,9 @@ int main(int argc, char *argv[]){
         pthread_join(threads[t], NULL);
     }
 
-    free(chop);
-
+    for(int i = 0; i < num_threads; i++){
+    	printf("philosopher %d has eaten %d times\n", i, count[i]);
+    }
     sem_destroy(&mutex);
 
     pthread_exit(NULL); 
