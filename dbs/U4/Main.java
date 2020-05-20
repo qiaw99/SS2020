ab3cc68f3a11a234b55adbfe48bd8d9f6d5e70fd
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-05-20 07:39:36 +0200

    Update Main.java

--- dbs/U4/Main.java
+++ dbs/U4/Main.java
@@ -1,38 +1,67 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Connection conn = null;
-        Statement statement = null;
-
+   public static void main(String args[]) {
+        Connection c = null;
+        Statement stmt = null;
         try {
-            // make the connection
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "testuser", "password");
-            System.out.println("Opened database successfully");
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "postgres", "qianli99");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully!");
+            
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+                      "(ID INT PRIMARY KEY     NOT NULL," +
+                      " NAME           TEXT    NOT NULL, " +
+                      " AGE            INT     NOT NULL, " +
+                      " ADDRESS        CHAR(50), " +
+                      " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+               + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
 
-            // statements execute queries
-            statement = conn.createStatement();
-            ResultSet results = statement.executeQuery("SELECT * FROM People WHERE age = 42");
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+               + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
 
-            // get the first result & unpack the data
-            results.next();
-            String content = results.getString("name");
-            System.out.println(content);
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+               + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
 
-            // close all active objects
-            results.close();
-            statement.close();
-            conn.close();
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+               + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
 
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.commit();
+            c.close();
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
-    }
+        System.out.println("Table created successfully!");
+   }
 }
