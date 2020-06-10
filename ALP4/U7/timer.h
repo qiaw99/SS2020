bf986bdf01e1f88252fe2d05fa4a7998d9bc1311
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-06-10 06:03:10 +0200

    Create timer.h

---
+++ ALP4/U7/timer.h
@@ -0,0 +1,14 @@
+// return the number of seconds that have elapsed since some point in the past.
+
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include <sys/time.h>
+
+#define GET_TIME(now) { \
+   struct timeval t; \
+   gettimeofday(&t, NULL); \
+   now = t.tv_sec + t.tv_usec/1000000.0; \
+}
+
+#endif
